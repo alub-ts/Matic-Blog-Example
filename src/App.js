358992f25a76e2a4e1fc1dad4abeb270dca4eac6
alvin lubetkin <alvin@terminal.co>
2019-12-17 12:41:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Portis from "@portis/web3";
+import Web3 from "web3";
+import { TerminalHttpProvider, SourceType } from "@terminal-packages/sdk";
+
+const portis = new Portis("PORTIS DEV ID", "maticAlpha");
+
+const web3 = new Web3(
+  new TerminalHttpProvider({
+    apiKey: "API_KEY", /// ADD YOUR TERMINAL API KEY HERE
+    projectId: "PROJECT_ID", /// ADD YOUR TERMINAL PROJECT ID HERE
+    customHttpProvider: portis.provider,
+    networkSource: "Matic",
+    source: SourceType.Portis,
+    host: portis.config.network.nodeUrl
+  })
+);
 
 function App() {
+  const [blockNumber, setBlockNumber] = useState(null);
+  const [acct, setAcct] = useState(null);
+
+  const getBlockNumber = () => {
+    web3.eth.getBlockNumber().then(res => setBlockNumber(res));
+  };
+
+  const sendTx = () => {
+    web3.eth.getAccounts().then(accounts => {
+      setAcct(accounts);
+      web3.eth
+        .sendTransaction({
+          from: accounts[0],
+          to: accounts[0],
+          value: web3.utils.toWei("0")
+        })
+        .then(console.log);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Matic Test</div>
+        <button onClick={() => getBlockNumber()}>Get Block Number</button>
+        <div>{`${blockNumber}`}</div>
+        <button onClick={() => sendTx()}>Send Transaction</button>
+        <div>{`${acct}`}</div>
       </header>
     </div>
   );
